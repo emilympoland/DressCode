bc81cdd4641114853dad1d6ff6079123306009e4
jeffreykim <jkjeffrey7@gmail.com>
2025-04-23 17:20:39 -0700

    updated database config

--- dresscode-backend/app/core/config.py
+++ dresscode-backend/app/core/config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 load_dotenv(dotenv_path=os.path.join(basedir, ".env.local"))
 
 d = os.getenv("DATABASE_URL")
--- dresscode-backend/app/core/database.py
+++ dresscode-backend/app/core/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, declarative_base
-from config import database_url
+from app.core.config import database_url
 
 
 engine = create_engine(database_url)
